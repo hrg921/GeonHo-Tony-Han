826f4b9bab099bbb072614839715eed305d102e1
hrg921 <hrg921@gmail.com>
2019-07-27 15:16:30 +0900

    feat(productivity): add db connection file

---
+++ packages/productivity/models/db.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+
+const host = process.env.POSTGRES_HOST;
+const port = process.env.POSTGRES_PORT;
+const username = process.env.POSTGRES_USERNAME;
+const password = process.env.POSTGRES_PASSWORD;
+const database = process.env.POSTGRES_PRODUCTIVITY_DATABASE;
+
+const connection = createConnection({
+  type: 'postgres',
+  host,
+  port,
+  username,
+  password,
+  database,
+});
+
+export default connection;
