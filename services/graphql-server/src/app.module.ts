60234bb8317e6ddc91a9b9ee33de2e47ebfda42d
hrg921 <hrg921@gmail.com>
2019-10-30 21:39:38 +0900

    feat(graphql-server): init graphql module

--- services/graphql-server/src/app.module.ts
+++ services/graphql-server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorModule } from './modules/author/author.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthorModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
