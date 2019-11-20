feafb98d9ac10759bc780401a0b032594c2bf3d2
hrg921 <hrg921@gmail.com>
2019-11-20 13:20:07 +0900

    feat(goal): add initial data

---
+++ services/goal-client/data.ts
@@ -0,0 +1,20 @@
+const data = {
+  lifeStyle: [
+    'Consult for my children companies',
+    'Start new business with my friend',
+    'Read books & Create something with my teams',
+  ],
+  timeline: [
+    {
+      time: '2019-12',
+      title: 'Start military service',
+      goals: [
+        'Earn 3,000,000₩ (evaluated at 2019.11.20) monthly life money with no work - Ice Americano 600 cups',
+        '1,000,000,000₩ investment money',
+      ],
+    },
+    { time: '2022-12', title: 'Finish military service' },
+    { time: '2025-12', title: 'Start my first business' },
+    { time: '2028-12', title: 'Start my second business' },
+  ],
+};
